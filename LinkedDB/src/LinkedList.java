9e3609dde344b7ea694a84f0949c2c9ae6125be4
Esteban <estebandcg1999@gmail.com>
2017-09-07 01:05:04 -0600

    [Fixed Insert and Delete]

--- LinkedDB/src/LinkedList.java
+++ LinkedDB/src/LinkedList.java
@@ -2,92 +2,90 @@ public class LinkedList {
 
 	private Node root;
 	private int largo;
-	
+
 	public LinkedList(){
 		this.root=null;
 		largo = 0;
 	}
 
-	
+
 
 	public void add(int valor){
-		
+
 		Node newNode = new Node();
 		newNode.setDato(valor);
-		
+
 		if (root==null){
-			root = newNode;			
+			root = newNode;
 		}
 		else{
 			Node current = root;
 			while(current.getNext()!=null){
 				current = current.getNext();
-				
+
 			}
-			
+
 			current.setNext(newNode);
-			
+
 		}
-		
+
 		largo++;
-		
+
 	}
-	
+
 	public void insert(int valor, int pos){
-		
+
 		Node newNode = new Node();
 		newNode.setDato(valor);
-		
-		if (pos>(largo)){ //**********************************
+
+		if (pos>(largo)){
 			System.out.println("Error: Excedió el largo de la lista");
-			return;
 		}
-			
-		Node current = root;
-		for (int i = 0; i < pos; i++){
-			current = current.getNext();
-			
+
+		else if (pos==0){
+			newNode.setNext(this.root);
+	        this.root = newNode;
 		}
-		
-		newNode.setNext(current.getNext());
-		current.setNext(newNode);
-		
+		else{
+			Node current = root;
+			for (int i = 0; i < pos - 1; i++){
+				current = current.getNext();
+			}
+			newNode.setNext(current.getNext());
+			current.setNext(newNode);
+		}
 		largo++;
-		
+
 	}
-	
+
 	public int getLargo() {
 		return largo;
 	}
-	
+
 	public void delete(int pos){
-		
-		
-		if (pos>(largo)){
+
+
+		if (pos>=(largo)){
 			System.out.println("Error: Excedió el largo de la lista");
-			return;
 		}
-			
-		if (pos == 0) {
+
+		else if (pos == 0) {
 			root = root.getNext();
-			return;
 		}
-		Node current = root;
-		for (int i = 0; i < pos - 1; i++){
-			current = current.getNext();
-			
+		else{
+			Node current = root;
+			for (int i = 0; i < pos - 1; i++){
+				current = current.getNext();
+
+			}
+			Node next = current.getNext();
+			current.setNext(next.getNext());
+			next.setNext(null);
 		}
-		if (current.getNext() == null) {
-			return;
-		}
-		Node next = current.getNext();
-		current.setNext(next.getNext());
-		next.setNext(null);
-		
 		largo--;
-		
+
 	}
-	
+
 	public void display() {
 		if(this.root == null){
 			return;
@@ -97,7 +95,7 @@ public class LinkedList {
             System.out.println(current.getDato());
             current = current.getNext();
         }
-        
+
 	}
     public Node find(int valor) {
         Node current = root;
@@ -109,9 +107,9 @@ public class LinkedList {
             }
         }
         return null;
-    
-	}    
-	
+
+	}
+
 	public static void main(String[] args){
         LinkedList l = new LinkedList();
         l.add(1);
@@ -133,8 +131,11 @@ public class LinkedList {
         System.out.println(l.find(6));
         System.out.println();
         System.out.println();
-        LinkedList l2 = new LinkedList();
-        l2.display();
+        l.insert(6, 4);
+        l.display();
+        System.out.println();
+        l.delete(3);
+        l.display();
 	}
-        
+
 }

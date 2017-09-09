9452556b261fa8f30f7b9facd5d74acab1caef77
Esteban <estebandcg1999@gmail.com>
2017-09-08 23:05:08 -0600

    [Pruebas con .get y .remove]

--- LinkedDB/src/JSONWrite.java
+++ LinkedDB/src/JSONWrite.java
@@ -1,12 +1,21 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-//import org.json.simple.JSONArray;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class JSONWrite {
 
-	public static void deleteJSON(String rute){
+	JSONObject doc;
+	FileWriter file;
+
+	public void deleteJSON(String rute){
 		File ruta = new File(rute);
 		if (ruta.delete())
 			System.out.println("Arhivo Eliminado");
@@ -14,50 +23,94 @@ public class JSONWrite {
 			System.out.println("Arhivo Eliminado");
 	}
 
-	@SuppressWarnings("unchecked")
-	public static void createJSON(String rute, LinkedList list){
-		JSONObject obj = new JSONObject();
-		int n = list.getLargo();
-		Node current1 = list.root;
-		Node current2 = current1.getNext();
-		for (int i=0; i < n/2; i++){
-			obj.put(current1.getDato(), current2.getDato());
-			if(i<(n/2)-1){
-				current1=current2.getNext();
-				current2=current1.getNext();
-			}
-		}
+	public void createJSON(String fileName){
+		doc = new JSONObject();
 		try {
-			FileWriter file = new FileWriter(rute);
-			file.write(obj.toJSONString());
+			file = new FileWriter("C:\\Users\\este0\\Desktop\\"+fileName+".json");
 			file.flush();
-			file.close();
 		} catch (IOException e) {
 		}
 	}
 
-	public static void main(String[] args) {
-
-		/*JSONArray list = new JSONArray();
-		list.add("tag 1");
-		list.add("tag 2");
-		list.add("tag 3");
-
-		obj.put("Tags", list);
-
-		JSONObject innerObj = new JSONObject();
-		innerObj.put("PostX","Escribir un JSON");
-		innerObj.put("PostY", "Leer un JSON");
-		innerObj.put("PostZ", "lalala");
-
-		obj.put("Posts",innerObj);*/
-		LinkedList list = new LinkedList();
-		list.add("Nombre");
-		list.add("Cadena");
-		list.add("Carnet");
-		list.add("Entero");
-		createJSON("C:\\Users\\este0\\Desktop\\prueba.json", list);
-
+	@SuppressWarnings("unchecked")
+	public void addObject(Object[] a, String name){
+		JSONObject obj = new JSONObject();
+		int n = a.length;
+		int j = 0;
+		int k = 1;
+		Object current1 = a[j];
+		Object current2 = a[k];
+		for (int i=0; i < n/2; i++){
+			obj.put(current1, current2);
+			if(i<(n/2)-1){
+				j+=2;
+				k+=2;
+				current1=a[j];
+				current2=a[k];
+			}
+		}
+		doc.put(name, obj);
 	}
 
+	public void deleteObject(){
+	}
+
+	public void writeJSON(){
+		try {
+			file.write(doc.toJSONString());
+			file.flush();
+		} catch (IOException e) {
+		}
+	}
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+
+		Object[] array= new Object[4];
+		array[0]="Carnet";
+		array[1]=2017097066;
+		array[2]="Carrera";
+		array[3]="Ing. en Computadores";
+		Object[] array2= new Object[4];
+		array2[0]="Carnet";
+		array2[1]=2012780453;
+		array2[2]="Carrera";
+		array2[3]="Ing. en Computacion";
+		JSONWrite doc = new JSONWrite();
+		doc.createJSON("Persona");
+		doc.addObject(array, "Esteban");
+		doc.addObject(array2, "Daniel");
+		doc.writeJSON();
+		System.out.println(doc.doc);
+
+
+		FileReader reader = new FileReader("C:\\Users\\este0\\Desktop\\Persona.json");
+		JSONParser jsonParser = new JSONParser();
+		JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+
+		JSONObject carnet = (JSONObject) jsonObject.get("Esteban");
+		System.out.println("Into Esteban carnet, : " + carnet.get("Carnet"));
+		System.out.println(jsonObject);
+		jsonObject.remove("Daniel");
+		System.out.println(jsonObject);
+
+		 /*JSONParser parser = new JSONParser();
+	        try {
+	            Object obj = parser.parse(new FileReader("C:\\Users\\este0\\Desktop\\Persona.json"));
+	            JSONObject jsonObject = (JSONObject)obj;
+
+	            System.out.println("Object from file:" + jsonObject);
+
+	            JSONObject id = (JSONObject)jsonObject.get("Esteban");
+	            System.out.println("Esteban:"+id);
+
+	            JSONArray list = (JSONArray)jsonObject.get("Daniel");
+				System.out.println(list);
+	            for (int i = 0; i < list.size(); i++) {
+	                System.out.println(i+ ": " + list.get(i));
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }*/
+
+	}
 }

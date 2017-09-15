a06b575c3f4b9c1cad6010112fa82ad227bb7084
Esteban <estebandcg1999@gmail.com>
2017-09-14 23:10:39 -0600

    [Nuevo: Manejo de Tipos de JSON]

--- LinkedDB/src/DataArchitecture/Store.java
+++ LinkedDB/src/DataArchitecture/Store.java
@@ -4,10 +4,43 @@ import java.io.File;
 
 public class Store {
 
+	private Document start;
+	private Document end;
 	private Store next;
 	private Store prev;
 	private File directorio;
 	private String nombre;
+	private String carpeta;
+
+	public Store() {
+		this.start=null;
+		this.end=null;
+		carpeta="";
+	}
+
+	public void add(TypeList types, String type, String ruta, Object[] atributosR){
+		if (types.exists(type)) {
+			Document newDocument = new Document();
+			String carpeta = this.carpeta+"\\"+ruta;
+			File directorio = new File(carpeta);
+			newDocument.setDirectorio(directorio);
+			newDocument.setCarpeta(carpeta);
+			newDocument.setNombre(ruta);
+			if ((start==null) && (end==null)){
+				start=newDocument;
+				end = start;
+			}
+			else{
+				Document current = end;
+				current.setNext(newDocument);
+				newDocument.setPrev(current);
+				end = newDocument;
+			}
+		}
+		else {
+			System.out.println("No existe" + type);
+		}
+	}
 
 	public Store getNext() {
 		return next;
@@ -32,6 +65,14 @@ public class Store {
 	}
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public String getCarpeta() {
+		return carpeta;
+	}
+
+	public void setCarpeta(String carpeta) {
+		this.carpeta = carpeta;
 	}
 
 }
--- LinkedDB/src/DataArchitecture/StoreList.java
+++ LinkedDB/src/DataArchitecture/StoreList.java
@@ -6,58 +6,54 @@ public class StoreList {
 
 	private Store start;
 	private Store end;
+	private String carpeta;
 
 	public StoreList(){
 		this.start=null;
 		this.end=null;
+		carpeta="C:\\Users\\este0\\Desktop\\Pruebas";
 	}
 
 	public void add(String ruta){
 		Store newStore = new Store();
-		File directorio = new File("C:\\Users\\este0\\Desktop\\"+ruta);
+		String carpeta = this.carpeta+"\\"+ruta;
+		File directorio = new File(carpeta);
 		newStore.setDirectorio(directorio);
+		newStore.setCarpeta(carpeta);
 		newStore.setNombre(ruta);
 		if (start==null){
 			start=newStore;
 			end = start;
-			start.setNext(end);
-			start.setPrev(end);
-			end.setNext(start);
-			end.setPrev(start);
 		}
 		else{
 			Store current = end;
 			current.setNext(newStore);
 			newStore.setPrev(current);
 			end = newStore;
-			end.setNext(start);
-			start.setPrev(end);
 		}
 	}
 
 	public void delete(String nombre){
 		Store current = find(nombre);
 		if (current == start) {
-			Store exStart = start;
-			start = start.getNext();
-			exStart.setNext(null);
-			exStart.setPrev(null);
-			start.setPrev(end);
-			end.setNext(start);
+			start=current.getNext();
+			start.setPrev(null);
+			if (current.getDirectorio().delete())
+				 System.out.println("El fichero " + current.getDirectorio() + " ha sido borrado correctamente");
+			else
+				 System.out.println("El fichero " + current.getDirectorio() + " no se ha podido borrar");
 		}
 		else if (current == end) {
-			Store newEnd = end.getPrev();
-			newEnd.setNext(start);
-			end.setPrev(null);
+			end=current.getPrev();
 			end.setNext(null);
-			end=newEnd;
-			start.setPrev(end);
+			if (current.getDirectorio().delete())
+				 System.out.println("El fichero " + current.getDirectorio() + " ha sido borrado correctamente");
+			else
+				 System.out.println("El fichero " + current.getDirectorio() + " no se ha podido borrar");
 		}
 		else {
 			Store anterior = current.getPrev();
 			Store siguiente = current.getNext();
-			current.setNext(null);
-			current.setNext(null);
 			anterior.setNext(siguiente);
 			siguiente.setPrev(anterior);
 			if (current.getDirectorio().delete())
@@ -72,10 +68,20 @@ public class StoreList {
 			return;
 		}
 		Store current = start;
-		do {
+		while (current != null) {
             System.out.println(current.getDirectorio());
             current=current.getNext();
-		} while (current != start);
+		}
+	}
+
+	public static void read(StoreList stores){
+		File d = new File(stores.carpeta);
+		if (d.exists()){
+			String[] listaArchivos=d.list();
+			for(int i=0; i<listaArchivos.length; i++){
+            	stores.add(listaArchivos[i]);
+        	}
+		}
 	}
 
 	public void write(){
@@ -83,31 +89,35 @@ public class StoreList {
 			return;
 		}
 		Store current = start;
-		do {
+		while (current != null) {
             current.getDirectorio().mkdir();
             current=current.getNext();
-		} while (current != start);
+		}
 	}
 
 	public Store find(String nombre) {
     	Store current = start;
-        do {
+    	while (current != null) {
         	if (current.getNombre()==nombre) {
                 return current;
             } else {
                 current = current.getNext();
             }
-        } while (current != start);
+        }
         return null;
 	}
 
 	public static void main(String[] args){
-		StoreList l = new StoreList();
-		l.add("Proyecto 1");
-		l.add("Proyecto 2");
-		l.add("Proyecto 3");
-		l.write();
-		l.delete("Proyecto 2");
-		l.display();
+		String[] atributos = {"Nombre", "Carnet", "Número"};
+		Object[] R = {"", 0};
+		Object[] N = {0};
+		String nombre = "Persona";
+		TypeList types = new TypeList();
+		types.add(atributos,R,N,nombre);
+		Object[] valores = {"Esteban",2017097066};
+		StoreList stores = new StoreList();
+		read(stores);
+		stores.display();
+		stores.find("Proyecto 1").add(types,"Persona", "Oscar",valores);
 	}
 }
